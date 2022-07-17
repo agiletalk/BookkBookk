3207e9a6c6eafc269d7fbbcff6614ec4aa66847e
chanju Jeon <chanju.jeon@gmail.com>
2022-07-17 19:49:50 +0900

    Initial Commit

---
+++ BookkBookk/BookkBookkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookkBookkApp.swift
+//  BookkBookk
+//
+//  Created by chanju Jeon on 2022/07/17.
+//
+
+import SwiftUI
+
+@main
+struct BookkBookkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
